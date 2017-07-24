066cff800e8728156ffdfe1fc42cd6d998170cf5
Arunan <arunans23@gmail.com>
2017-07-24 17:44:04 +0530

    refactor code

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
 
-var Examples = React.createClass({
-  render: function(){
-    return(
-      <h3>Examples component</h3>
-    );
-  }
-});
+// var Examples = React.createClass({
+//   render: function(){
+//     return(
+//       <h3>Examples component</h3>
+//     );
+//   }
+// });
+
+var Examples = (props) => {
+  return(
+    <h1>Examples component</h1>
+  )
+};
 
 module.exports = Examples;
