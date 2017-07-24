2ec6d46ab6f957b7de64e98c619a68d3d6eb95a3
Arunan <arunans23@gmail.com>
2017-07-24 22:49:52 +0530

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,8 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//   render: function(){
-//     return(
-//       <h3>Examples component</h3>
-//     );
-//   }
-// });
-
 var Examples = (props) => {
   return(
-    <h1>Examples component</h1>
+    <h1>Examples</h1>
   )
 };
 
