b22129b6792542c20a4e8a659719ad82268af6b1
Arunan <arunans23@gmail.com>
2017-07-26 10:34:22 +0530

    add links to examples page and style it

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h1>Examples</h1>
-      <p>This is the examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to = '/?location=Philadelphia'>
+            Philadelphia, PA
+          </Link>
+        </li>
+        <li>
+          <Link to = '/?location=Colombo'>
+            Colombo, LK
+          </Link>
+        </li>
+      </ol>
     </div>
   )
 };
