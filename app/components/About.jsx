3ad03c569f978d3027ebe4388d26cf3cb2297854
Arunan <arunans23@gmail.com>
2017-07-26 10:44:07 +0530

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props) =>{
   return(
     <div>
-      <h3>About</h3>
-      <p>This is the about page</p>
+      <h3 className="text-center">About</h3>
+      <p>This system was developed for educational purposes</p>
+      <br/>
+      <p>You can find the github repo for the project in
+        <a href="https://github.com/arunans23/weather-app"> here</a>
+      .</p>
+      <br/>
     </div>
   )
 };
