d46291ef1b3488615425f1d97bea3d719725f89e
Arunan <arunans23@gmail.com>
2017-07-27 15:20:21 +0530

    add custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) =>{
   return(
     <div>
-      <h3 className="text-center">About</h3>
+      <h3 className="text-center page-title">About</h3>
       <p>This system was developed for educational purposes</p>
       <br/>
       <p>You can find the github repo for the project in
